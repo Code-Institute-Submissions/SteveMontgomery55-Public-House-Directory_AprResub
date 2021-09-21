53f3fc7166330285b3bfb147fa1a1350586698f9
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2021-09-21 00:18:31 +0000

    create add pub page

--- app.py
+++ app.py
@@ -96,6 +96,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_pub")
+def add_pub():
+    return render_template("add_pub.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
